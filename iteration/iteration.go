9557597d21be84e4ab0d28387255d70181122208
Eric Brookman <eric.brookman@bbitsolutions.us>
2024-08-29 21:49:41 -0400

    refactored with const

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,9 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 // Repeat takes a character and repeats it 5 times
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += character
 	}
 	return repeated
