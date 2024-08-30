88c669c9e1317a3d49be82d932d9bb9078ef1bb7
Eric Brookman <eric.brookman@bbitsolutions.us>
2024-08-29 22:09:10 -0400

    Added range to arrays

---
+++ arrays_and_slices/sum.go
@@ -0,0 +1,10 @@
+package arrays_and_slices
+
+// Sum takes an array of numbers and returns the sum of them
+func Sum(numbers [5]int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
