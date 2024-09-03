fb1f12757134180cd5b5c8b0b493a73e6adab3ec
Eric Brookman <eric.brookman@bbitsolutions.us>
2024-09-03 11:36:10 -0400

    Initial build of pointers

---
+++ pointers_and_errors/wallet.go
@@ -0,0 +1,31 @@
+package wallet
+
+import (
+	"fmt"
+)
+
+type Wallet struct {
+	balance Bitcoin
+}
+
+type Stringer interface {
+	String() string
+}
+
+type Bitcoin int
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	w.balance -= amount
+}
